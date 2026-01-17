e2ab261fc68e7099304d5846b37808a34efdea94
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-17 14:32:08 -0300

    Funções com retorno

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-//let titulo = document.querySelector("h1");
-//titulo.innerHTML = "Hora do desafio";
-
-//let paragrafo = document.querySelector ("p");
-//paragrafo.innerHTML= "Escolha um número entre 1 e 10";
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -13,5 +9,9 @@ exibirTextoNaTela ("h1", "Hora do desafio");
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log("Cite uma cidade brasileira");
+    console.log(numeroSecreto);
     }
+
+function gerarNumeroAleatorio(params) {
+  return parseInt(Math.random() * 10 + 1);
+}
