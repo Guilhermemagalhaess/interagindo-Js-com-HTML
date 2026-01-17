cdfd33bb4caf1625c3ff0e61b7063444e00ce975
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-17 14:03:58 -0300

    Funções com parâmetros

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Jogo do número aleatório";
+//let titulo = document.querySelector("h1");
+//titulo.innerHTML = "Hora do desafio";
 
-let paragrafo = document.querySelector ("p");
-paragrafo.innerHTML= "Escolha um número entre 1 e 10";
+//let paragrafo = document.querySelector ("p");
+//paragrafo.innerHTML= "Escolha um número entre 1 e 10";
 
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela ("h1", "Hora do desafio");
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log("O botão foi clicado")
-}
+    console.log("Cite uma cidade brasileira");
+    }
